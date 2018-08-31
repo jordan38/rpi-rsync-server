e74ed4793d8d6f8d4a92d5156ddc1f3a02f07824
jcontent <jcontent@tennaxia.com>
2018-08-31 15:23:58 +0200

    Extract sshd_config in extra file

--- docker-entrypoint.d/rsync.sh
+++ docker-entrypoint.d/rsync.sh
@@ -5,6 +5,10 @@ touch rsyncd.secrets
 chmod 600 /etc/rsyncd.secrets
 echo "$LOGIN:$PASSWORD" > /etc/rsyncd.secrets
 
+useradd $LOGIN -m -G users
+passwd $PASSWORD
+
+
 mkdir -p /rsync/backup/data
 mkdir -p /var/log/rsync
 cd /var/log/rsync/
--- docker-entrypoint.d/sshd.sh
+++ docker-entrypoint.d/sshd.sh
@@ -4,13 +4,6 @@
 
 set -e
 
-sed -i s/#PermitRootLogin.*/PermitRootLogin\ yes/ /etc/ssh/sshd_config
-sed -ie 's/#Port 22/Port 22/g' /etc/ssh/sshd_config
-sed -ri 's/#HostKey \/etc\/ssh\/ssh_host_key/HostKey \/etc\/ssh\/ssh_host_key/g' /etc/ssh/sshd_config
-sed -ir 's/#HostKey \/etc\/ssh\/ssh_host_rsa_key/HostKey \/etc\/ssh\/ssh_host_rsa_key/g' /etc/ssh/sshd_config
-sed -ir 's/#HostKey \/etc\/ssh\/ssh_host_dsa_key/HostKey \/etc\/ssh\/ssh_host_dsa_key/g' /etc/ssh/sshd_config
-sed -ir 's/#HostKey \/etc\/ssh\/ssh_host_ecdsa_key/HostKey \/etc\/ssh\/ssh_host_ecdsa_key/g' /etc/ssh/sshd_config
-sed -ir 's/#HostKey \/etc\/ssh\/ssh_host_ed25519_key/HostKey \/etc\/ssh\/ssh_host_ed25519_key/g' /etc/ssh/sshd_config
 /usr/bin/ssh-keygen -A
 ssh-keygen -t rsa -b 4096 -f  /etc/ssh/ssh_host_key
 
