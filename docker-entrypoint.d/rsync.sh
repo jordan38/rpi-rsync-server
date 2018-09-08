28e3d9df8d89cefe8a539f813cb2a7fa77734a4b
jordan38 <jordan.content@outlook.fr>
2018-09-08 22:26:13 +0200

    Modify config

--- docker-entrypoint.d/rsync.sh
+++ docker-entrypoint.d/rsync.sh
@@ -2,11 +2,14 @@
 
 cd /etc/
 touch rsyncd.secrets
-chmod 600 /etc/rsyncd.secrets
+chmod 400 /etc/rsyncd.secrets
 echo "$LOGIN:$PASSWORD" > /etc/rsyncd.secrets
+addgroup -S rsync && adduser -S -G $LOGIN rsync
 
 mkdir -p /rsync/backup/data
 mkdir -p /var/log/rsync
+chown -R $LOGIN:rsync /rsync/
+chmod -R a+rw /rsync/
 cd /var/log/rsync/
 touch rsync.log
 ln -sf /dev/stdout /var/log/rsync/rsync.log
